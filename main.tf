7449f0660e2a7cb93551ef942552af71cc5a26ff
rajivkumar2707 <rajivkumar27@gmail.com>
2026-04-13 15:00:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+variable "employees" {
+  type = set(object({
+    name  = string
+    role  = string
+    email = string
+  }))
+   default = [
+    {
+      name  = "Alice"
+      role  = "HR"
+      email = "alice@example.com"
+    },
+    {
+      name  = "Bob"
+      role  = "Recruiter"
+      email = "bob@example.com"
+    }
+  ]
+}
+ resource "null_resource" "employees" {
+  for_each = {
+    for emp in var.employees : emp.email => emp
+  }
+ 
+  triggers = {
+    name = each.value.name
+    role = each.value.role
+  }
+}
+output "printvalue" {
+value = null_resource.employees
+}
